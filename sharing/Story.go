ca663596ac0f2f8de468392497e88273c49fd6df
sowens-csd <stephen.owens@thoughtwire.com>
2021-07-30 16:31:03 -0400

    feat: make TItle on SharedStory optional

--- sharing/Story.go
+++ sharing/Story.go
@@ -36,7 +36,7 @@ type StoryGroup struct {
 type SharedStory struct {
 	StoryID        string       `json:"id" dynamodbav:"id"`
 	AlbumReference string       `json:"albumReference" dynamodbav:"albumReference"`
-	Title          string       `json:"title" dynamodbav:"title"`
+	Title          string       `json:"title,omitempty" dynamodbav:"title,omitempty"`
 	Content        string       `json:"content,omitempty" dynamodbav:"content"`
 	Version        string       `json:"version" dynamodbav:"version"`
 	BaseVersion    string       `json:"baseVersion" dynamodbav:"baseVersion"`
