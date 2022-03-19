526df69d2ba926948207f3687ce5e6640a774d7a
sowens-csd <stephen.owens@thoughtwire.com>
2022-03-19 10:51:35 -0400

    feat: first functionality for websocket

--- awsproxy/FTContext.go
+++ awsproxy/FTContext.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambdacontext"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
@@ -105,6 +106,42 @@ func NewFromContextAndJWT(ctx context.Context, request Request) (FTContext, *Res
 	lambdaContext, _ := lambdacontext.FromContext(ctx)
 	requestID := lambdaContext.AwsRequestID
 	claims := request.RequestContext.Authorizer
+	if nil == claims {
+		requestLogger := log.With().Str("request_id", requestID).Logger()
+		requestLogger.Error().Msg("claims null: ")
+		// requestLogger.Error().Msg(request.RequestContext.Authorizer)
+		errResp := HandleError(fmt.Errorf("No claims in the request"), requestLogger)
+		return FTContext{}, &errResp
+	}
+	userID := claims[UserIDContextField].(string)
+	email := claims[EmailContextField].(string)
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		panic("Could not load AWS config")
+	}
+	svc := dynamodb.NewFromConfig(cfg)
+	setLogLevel()
+	requestLogger := log.With().Str(requestIDLogField, requestID).Str(userIDLogField, userID).Logger()
+	return FTContext{
+		Context:       ctx,
+		RequestID:     requestID,
+		UserID:        userID,
+		Email:         email,
+		DBSvc:         svc,
+		RequestLogger: requestLogger,
+		EmailSvc:      &sesEmailSender{},
+	}, nil
+}
+
+// NewFromContextAndJWT create a new FTContext from an inbound proxy context
+// that was authorized through the custom JWT authorizer.
+//
+// The JWT authorizer adds new claims with the internal Folktells user id and
+// email address to the request.
+func NewFromWebsocketContextAndJWT(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (FTContext, *Response) {
+	lambdaContext, _ := lambdacontext.FromContext(ctx)
+	requestID := lambdaContext.AwsRequestID
+	claims := request.RequestContext.Authorizer.(map[string]interface{})
 	if nil == claims {
 		requestLogger := log.With().Str("request_id", requestID).Logger()
 		requestLogger.Error().Msg("claims null: ")
